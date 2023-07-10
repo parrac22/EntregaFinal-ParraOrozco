b04ef60d19a3a4556a4a5143765c1f804e16b46c
Carlos Parra <78557164+parrac22@users.noreply.github.com>
2023-07-09 23:57:31 -0500

    grafico agregado

--- js/home.js
+++ js/home.js
@@ -2,9 +2,18 @@ const crearId = () => Math.random().toString(36).substr(2, 9)
 
 const numberInput = document.querySelector('#numberInput')
 const dropDownMenu = document.querySelector('#dropDownMenu')
-
 const scorecardTotalExpenditure = document.querySelector('#totalGastado')
+const donutChartContainer = document.querySelector(".donutChartCanvas")
+const arrayColores = []
+let donutChart
 
+const llenarArrayColores = async ()=> {
+    const response = await fetch("js/colores.json")
+    const data = await response.json()
+    data.colores.forEach((color) => {
+        arrayColores.push(color)
+    })
+}
 
 
 const construirCategoria = (categoria)=> {
@@ -50,7 +59,55 @@ const calcularTotalGastos = (arrayLedger) => {
     return total || 0
 }
 
+const obtenerObjetoGastoCategoria = (arrayLedger) => {
+    ledger = new Ledger(arrayLedger)
+    objetoGastoCategoria= ledger.calcularGastoPorCategoria() || undefined
+    return objetoGastoCategoria
+}
 
+const construirDataDonutChart = (objetoGastoCategoria) => {
+    etiquetas = Object.keys(objetoGastoCategoria)
+    valores = Object.values(objetoGastoCategoria)
+    colores = arrayColores.slice(0,etiquetas.length)
+
+    const chartData = {
+        labels: etiquetas,
+        datasets: [{
+            data:valores,
+            backgroundColor: colores
+        }]
+    }
+    return chartData
+}
+
+const generarDonutChart = () => {
+    objetoGastoCategoria = obtenerObjetoGastoCategoria(obtenerLedgerLocalStorage())
+    if (objetoGastoCategoria) {
+        datosDonutChart = construirDataDonutChart(objetoGastoCategoria)
+        const donutOptions = {
+            cutout: '80%',
+            plugins: {            
+                legend : {
+                    display: true,
+                    position: 'right',
+                    labels: {
+                        fontSize: 20,
+                        fontStyle: 'bold'
+                    }
+                }
+            }
+        }
+        if (donutChart) {
+            donutChart.destroy()
+        }
+        donutChart = new Chart(donutChartContainer , {
+            type: 'doughnut',
+            data: datosDonutChart,
+            options: donutOptions
+        })
+    }
+
+}
 
 const activarClickEnBotonGuardar = ()=> {
     const saveButton = document.querySelector('.saveButton')
@@ -61,21 +118,26 @@ const activarClickEnBotonGuardar = ()=> {
         scorecardTotalExpenditure.textContent = `Total gastado: $${totalGasto}`
         dropDownMenu.value = 'Selecciona la categoria de gasto'
         numberInput.value = ''
+        generarDonutChart()
     }) 
 }
 
 const llenarTotalGastadoScorecard = () => {
     arrayLedger = obtenerLedgerLocalStorage()
     totalGasto = calcularTotalGastos(arrayLedger)
-    scorecardTotalExpenditure.textContent = `Total gastado: $${totalGasto}` 
+    scorecardTotalExpenditure.textContent = `Total gastado: $${totalGasto.toLocaleString()}` 
 }
 
 llenarDropDown()
 llenarTotalGastadoScorecard()
 
-const realizarCargaInicialApp = () => {
+const realizarCargaInicialApp = async () => {
     activarClickEnBotonGuardar()
+    await llenarArrayColores()
+    generarDonutChart();
+    
 }
 
 realizarCargaInicialApp()
+
 
